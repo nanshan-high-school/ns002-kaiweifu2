f790854b5c4321522af6fadf40369e7f90fd46e0
KaKakaKakkkKAkkakkAkkkAkAAkakkakakakakAkAkAkAkkA <45233906+kaiweifu2@users.noreply.github.com>
2019-03-20 23:59:50 +0800

    Create 未完成的媒婆.cpp

---
+++ 未完成的媒婆.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+int main() {
+  int n;
+  queue<int>q;
+  
+  cin>>n;
+  for(int i = 0;i < n/2; i++){
+    char people;
+    cout<<"please enter peoplename:";
+    cin>>people;
+    for(int n = 0; n < i; n++){
+      
+    }
+  }
+}
